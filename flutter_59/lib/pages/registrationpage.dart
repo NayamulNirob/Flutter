683d1ab8229d84c9c77aed4396caa3fd82927eb4
NayamulNirob <158471735+NayamulNirob@users.noreply.github.com>
2024-10-27 23:04:07 +0600

    Create registrationpage.dart

---
+++ flutter_59/lib/pages/registrationpage.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+
+class Registrationpage extends StatefulWidget {
+  const Registrationpage({super.key});
+
+  @override
+  State<Registrationpage> createState() => _RegistrationpageState();
+}
+
+class _RegistrationpageState extends State<Registrationpage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
