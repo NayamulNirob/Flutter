bb2c1904944bf69d40e45cc051f9db775ae6112a
NayamulNirob <nayamulislam@gmail.com>
2025-01-14 05:49:18 +0600

    Update splash_screen.dart

--- my_vault/lib/views/SplashScreen/splash_screen.dart
+++ my_vault/lib/views/SplashScreen/splash_screen.dart
@@ -13,9 +13,18 @@ class _SplashViewState extends State<SplashView> {
     return  MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        appBar: AppBar( backgroundColor: Colors.tealAccent, title: Center(child: Text("App Bar")),
-      )
-    )
+        appBar: AppBar( 
+          backgroundColor: Colors.tealAccent, 
+          title: Center(child: Text("App Bar")),
+      ), 
+        body:Center( child: Text("Body",
+          style: TextStyle(fontSize: 150,color: Colors.teal),)),
+        floatingActionButton: FloatingActionButton(
+          onPressed: ()=>{},
+          backgroundColor: Colors.blueAccent,
+          child: Icon(Icons.ac_unit,color: Colors.amber,),
+        ),
+    ),
     );
 
   }
