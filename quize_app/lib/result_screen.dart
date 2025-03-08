583d777d4df1e10b48af6be42bac6b2211f63190
NayamulNirob <nayamulislam@gmail.com>
2025-03-09 03:12:13 +0600

    video:3.33

--- quize_app/lib/result_screen.dart
+++ quize_app/lib/result_screen.dart
@@ -1,14 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:quize_app/data/questions.dart';
+import 'package:quize_app/question_summary.dart';
 
-class ResultScreen extends StatelessWidget{
-  const ResultScreen({super.key,required this.choosenAnswer });
+class ResultScreen extends StatelessWidget {
+  const ResultScreen({super.key, required this.choosenAnswer});
 
   final List<String> choosenAnswer;
-  
-  // final dynamic resetQuiz;
+
+  List<Map<String, Object>> getSummary() {
+    final List<Map<String, Object>> summary = [];
+
+    for (var i = 0; i < choosenAnswer.length; i++) {
+      summary.add({
+        'question_index': i,
+        'question': questions[i].text,
+        'correct_answer': questions[i].answers[0],
+        'user_answer': choosenAnswer[i],
+      });
+    }
+    return summary;
+  }
 
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Center(
       child: SizedBox(
         width: double.infinity,
@@ -18,13 +32,10 @@ class ResultScreen extends StatelessWidget{
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               Text('You did it! Your score is '),
-              SizedBox(height: 30,),
-              Text('List Of Answer and questions'),
-              SizedBox(height: 30,),
-              ElevatedButton(
-                onPressed: (){},
-                child: Text('Restart Quiz'),
-              ),
+              SizedBox(height: 30),
+              QuestionSummary(summaryData: getSummary()),
+              SizedBox(height: 30),
+              ElevatedButton(onPressed: () {}, child: Text('Restart Quiz')),
             ],
           ),
         ),
