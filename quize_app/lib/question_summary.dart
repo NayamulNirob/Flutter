a64990fdf263f2ad485dfa0d61092a39f42e5ee8
NayamulNirob <nayamulislam@gmail.com>
2025-03-10 12:56:39 +0600

    luncher_icon Added

--- quize_app/lib/question_summary.dart
+++ quize_app/lib/question_summary.dart
@@ -14,44 +14,62 @@ class QuestionSummary extends StatelessWidget {
         child: Column(
           children:
               summaryData.map((data) {
-                final bool isCorrect = data['user_answer'] == data['correct_answer'];
-                return Row(
-                  children: [
-                    Text(((data['question_index'] as int) + 1).toString(),style: TextStyle(color: Colors.white),),
-                    Expanded(
-                      child: Column(
-                        children: [
-                          Text(
-                            data['question'] as String,
-                            style: GoogleFonts.lato(
-                              color: Colors.white,
-                              fontSize: 16,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                          SizedBox(height: 5),
-                          Text(
-                            // data['user_answer'] as String,
-                            'Your Answer: ${data['user_answer']}',
-                            style: GoogleFonts.roboto(
-                              color: isCorrect
-                              ? Color.fromARGB(255, 111, 231, 63)
-                              : const Color.fromARGB(255, 248, 1, 1),
-                            ),
-                          ),
-                          Text(
-                            // data['correct_answer'] as String,
-                            'Correct Answer: ${data['correct_answer']}',
-                            style:  GoogleFonts.roboto(
-                              color: Color.fromARGB(255, 111, 231, 63),
-                              fontWeight: FontWeight.w500,
-                            ),
-                          ),
-                          Divider(color: Colors.white),
-                        ],
+                final bool isCorrect =
+                    data['user_answer'] == data['correct_answer'];
+                return Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 8.0),
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Container(
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(100),
+                          color: Colors.blue, // Add a background color
+                        ),
+                        width: 30,
+                        height: 30,
+                        alignment: Alignment.center,
+                        child: Text(
+                          ((data['question_index'] as int) + 1).toString(),
+                          style: TextStyle(color: Colors.white),
+                        ),
                       ),
-                    ),
-                  ],
+                      Expanded(
+                        child: Column(
+                          children: [
+                            Text(
+                              data['question'] as String,
+                              style: GoogleFonts.lato(
+                                color: Colors.white,
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            SizedBox(height: 5),
+                            Text(
+                              // data['user_answer'] as String,
+                              'Your Answer: ${data['user_answer']}',
+                              style: GoogleFonts.roboto(
+                                color:
+                                    isCorrect
+                                        ? Color.fromARGB(255, 111, 231, 63)
+                                        : const Color.fromARGB(255, 248, 1, 1),
+                              ),
+                            ),
+                            Text(
+                              // data['correct_answer'] as String,
+                              'Correct Answer: ${data['correct_answer']}',
+                              style: GoogleFonts.roboto(
+                                color: Color.fromARGB(255, 111, 231, 63),
+                                fontWeight: FontWeight.w500,
+                              ),
+                            ),
+                            Divider(color: Colors.white),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
                 );
               }).toList(),
         ),
