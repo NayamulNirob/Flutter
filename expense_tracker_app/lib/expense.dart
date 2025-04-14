dfe16a9ab268be43916747e7829fde4b674bed24
NayamulNirob <158471735+NayamulNirob@users.noreply.github.com>
2025-04-15 00:48:53 +0600

    expensetracker

---
+++ expense_tracker_app/lib/expense.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class Expense extends StatefulWidget{
+  const Expense({super.key});
+
+  @override
+  State<Expense> createState() => _ExpenseState();
+}
+
+class _ExpenseState extends State<Expense> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        actions: [IconButton(onPressed: ()=>{}, icon:Icon(Icons.add))],
+        backgroundColor: Colors.blue,
+        ),
+      body:Center(
+        child: Column(
+          children: [
+            Text("Hello"),
+            Text("Hey"),
+            Text("Hi")
+          ],
+        ),
+      ),
+    );
+  }
+}
