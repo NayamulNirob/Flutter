b4f222daa916818b720eeef2cfc49b89b98cb051
NayamulNirob <nayamulislam@gmail.com>
2025-04-21 20:40:43 +0600

    vid:5.4

--- expense_tracker_app/lib/models/expense.dart
+++ expense_tracker_app/lib/models/expense.dart
@@ -2,18 +2,23 @@ import 'package:ulid/ulid.dart';
 
 var uuid = Ulid();
 
+enum Category {
+  food,
+  travel,
+  leisure,
+  work,
+}
+
 class Expense {
- Expense({
-    required this.title,
-    required this.amount,
-    required this.date,
-  }) : id = uuid.toUuid();
+  Expense({
+    required this.category,
+    required this.title, 
+    required this.amount, 
+    required this.date}): id = uuid.toUuid();
 
-
- final String id;
- final String title;
- final double amount;
- final DateTime date;
-
-  
+  final String id;
+  final String title;
+  final double amount;
+  final DateTime date;
+  final Category category;
 }
